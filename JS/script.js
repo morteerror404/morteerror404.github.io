38d21479ca9bf4fdef4bf1926479b99ad1098c47
morteerror404 <frotatv@gmail.com>
2024-11-16 20:19:14 -0300

    Primeira ideia de menu, utilizando botoes

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
 /* DECLARANDO VARIAVEIS DO MENU */
 
 var home;
-var projects; 
-var history;
 var employments;
+var history;
+var projects; 
 var other;
 
 var help;
@@ -20,6 +20,23 @@ var help;
 
 function selectPage(home, projects, history, employments,other,help) {
  
-    
+    var mybutton = document.getElementById("myBtn");
+
+// quando rolar a página 20px do topo para baixo, o botão será exibido
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// // quando clicar no botão, a página rola para o topo do documento
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
     
 }
