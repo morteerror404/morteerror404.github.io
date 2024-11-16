c11efe184cc89b61fbd916296acea3245a14dd57
morteerror404 <frotatv@gmail.com>
2024-11-16 18:37:09 -0300

    Initial project version

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+/* DECLARANDO VARIAVEIS DO MENU */
+
+var home;
+var projects; 
+var history;
+var employments;
+var other;
+
+var help;
+
+/* DECLARANDO VARIAVEIS DO BODY */
+
+
+
+
+/* DECLARANDO VARIAVEIS DO CTF */
+
+
+/* FUNCOES MENU NAVEGACAO */
+
+function selectPage(home, projects, history, employments,other,help) {
+ 
+    
+    
+}
